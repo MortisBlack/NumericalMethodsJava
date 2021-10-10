9e8ca15e969cb55a2251b08887858f28fc046bfa
MortisBlack <jesus.urrego.iuc@gmail.com>
2021-10-09 20:06:55 -0700

    Gauss Seidel Update

--- gaussSeidel/src/gaussseidel/GaussSeidel.java
+++ gaussSeidel/src/gaussseidel/GaussSeidel.java
@@ -3,105 +3,137 @@ package gaussseidel;
 import java.util.Scanner;
 
 public class GaussSeidel {
-    
-    public static void GaussSeidel(float[][] matrix){        
-        float[] datos = new float[6];
 
-        datos[0] = 0f;
-
-        datos[1] = 0f;
-
-        datos[2] = 0f;
-        
-
-        datos[3] = 12345f;
-
-        datos[4] = 12345f;
-
-        datos[5] = 12345f;
-        
-        float eamax = 0.001f;
-        
-        System.out.println("Matriz inicial:");
-
-        System.out.printf("| %8.4f  %8.4f %8.4f | = | %8.4f |\n", matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]);
-        System.out.printf("| %8.4f  %8.4f %8.4f | = | %8.4f |\n", matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]);
-        System.out.printf("| %8.4f  %8.4f %8.4f | = | %8.4f |\n", matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3]);
-        System.out.println("");
-        
-
-        System.out.printf("%5s%11s%11s%11s%9s%9s\n","X1","X2","X3","eaX1","eaX2","eaX3");
-
-        System.out.printf("%8.4f   %8.4f   %8.4f\n", datos[0], datos[1], datos[2]);
-        while(Math.abs(datos[3]) > eamax && Math.abs(datos[4]) > eamax && Math.abs(datos[5]) > eamax){
-            operacion(matrix, datos);
-
-            System.out.printf("%8.4f   %8.4f   %8.4f  %8.4f  %8.4f  %8.4f\n", datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]);
-        }
-        
-
-        operacion(matrix, datos);
-        System.out.printf("%8.4f   %8.4f   %8.4f  %8.4f  %8.4f  %8.4f\n", datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]);
-        
-        despliegaSolucion(datos);
-    }
-
-    public static void operacion(float[][] matrix, float[] datos){
-        float x1v = datos[0];
-        float x2v = datos[1];
-        float x3v = datos[2];
-        
-
-        datos[0] = (matrix[0][3] - (matrix[0][1] * datos[1]) - (matrix[0][2] * datos[2])) / matrix[0][0];
-
-        datos[3] = (x1v - datos[0]) / datos[0];
-        
-
-        datos[1] = (matrix[1][3] - (matrix[1][0] * datos[0]) - (matrix[1][2] * datos[2])) / matrix[1][1];
-
-        datos[4] = (x2v - datos[1]) / datos[1];
-        
-
-        datos[2] = (matrix[2][3] - (matrix[2][0] * datos[0]) - (matrix[2][1] * datos[1])) / matrix[2][2];
-
-        datos[5] = (x3v - datos[2]) / datos[2];
-    }
-    
-
-    public static void despliegaSolucion(float[] datos) {
-        System.out.println("-----------------------------");
-        System.out.println("");
-        System.out.printf("Las respuestas del sistema de ecuaciones dado son (aproximadamente dado el error del 0.1%1c):\n"
-                + "x1 = %8.4f\nx2 = %8.4f\nx3 = %8.4f\n", '%', datos[0], datos[1], datos[2]);
-    }
-    
-    public static void main(String[] args) {
+    public static double[][] leeEcuaciones() {
         Scanner scr = new Scanner(System.in);
+        double[][] matriz;
+        boolean flag = true;
+        int ecuaciones = 0;
 
-        float[][] matrix = new float[3][7];
+        System.out.println("¿Cuantas ecuaciones tiene el ejercicio? (máximo 10):");
+        do {
+            ecuaciones = scr.nextInt();
+            if (ecuaciones <= 0 && ecuaciones >= 10) {
+                System.out.println("Numero incorrecto, ingrese otro numero.");
+            }
+        } while (ecuaciones <= 0 && ecuaciones >= 10);
 
-        System.out.println("");
-        System.out.println("Ingrese los valores de la primer ecuación");
-        System.out.print("X1 -> "); matrix[0][0] = scr.nextFloat();
-        System.out.print("X2 -> "); matrix[0][1] = scr.nextFloat();
-        System.out.print("X3 -> "); matrix[0][2] = scr.nextFloat();
-        System.out.print("=  -> "); matrix[0][3] = scr.nextFloat();
-
-        System.out.println("");
-        System.out.println("Ingrese los valores de la segunda ecuación");
-        System.out.print("X1 -> "); matrix[1][0] = scr.nextFloat();
-        System.out.print("X2 -> "); matrix[1][1] = scr.nextFloat();
-        System.out.print("X3 -> "); matrix[1][2] = scr.nextFloat();
-        System.out.print("=  -> "); matrix[1][3] = scr.nextFloat();
+        matriz = new double[ecuaciones][ecuaciones + 1];
 
         System.out.println("");
-        System.out.println("Ingrese los valores de la tercera ecuación");
-        System.out.print("X1 -> "); matrix[2][0] = scr.nextFloat();
-        System.out.print("X2 -> "); matrix[2][1] = scr.nextFloat();
-        System.out.print("X3 -> "); matrix[2][2] = scr.nextFloat();
-        System.out.print("=  -> "); matrix[2][3] = scr.nextFloat();
-                 
-        GaussSeidel(matrix);
+
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("Valores de la fila " + (i + 1) + ":");
+            for (int j = 0; j < matriz[i].length - 1; j++) {
+                System.out.printf("Ingrese el valor del coeficiente X%2s:\n", i + 1);
+                matriz[i][j] = scr.nextDouble();
+            }
+            System.out.println("Ingrese el valor del término independiente:");
+            matriz[i][matriz[i].length - 1] = scr.nextDouble();
+            System.out.println("");
+        }
+        return matriz;
     }
-    
+
+    public static void GaussSeidel(double[][] matriz) {
+
+        System.out.println("Sistema de ecuaciones lineales simultáneas indicado inicial:");
+        impresionInicial(matriz);
+        System.out.println("");
+
+        double[] soluciones = new double[matriz.length];
+        double suma;
+        double[] div = new double[matriz.length];
+        double[] ea = new double[matriz.length];
+        double eaAux = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length - 1; j++) {
+                if (i == j) {
+                    div[i] = matriz[i][j];
+                }
+            }
+        }
+
+        boolean flag = false;
+
+        impresionCabecera(soluciones);
+
+        while (!flag) {
+            for (int i = 0; i < matriz.length; i++) {
+                suma = 0;
+                eaAux = soluciones[i];
+                for (int j = 0; j < matriz[i].length - 1; j++) {
+                    if (i != j) {
+                        suma += (matriz[i][j] * -1) * soluciones[j];
+                    }
+                }
+                soluciones[i] = (matriz[i][matriz[i].length - 1] + suma) / div[i];
+                ea[i] = (soluciones[i] - eaAux) / soluciones[i];
+            }
+
+            impresionLineas(soluciones, ea);
+
+            for (int i = 0; i < ea.length + 1; i++) {
+                if ((i) == ea.length) {
+                    flag = true;
+                } else if (Math.abs(ea[i]) > Math.abs(0.0001)) {
+                    break;
+                }
+            }
+        }
+        System.out.println("");
+
+        despliegaSolucion(soluciones);
+    }
+
+    public static void impresionInicial(double[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < matriz[i].length - 2; j++) {
+                System.out.printf("(%10.4f) X%2s + ", matriz[i][j], (j + 1));
+            }
+            System.out.printf("(%10.4f) X%2s ", matriz[i][matriz[i].length - 2], matriz[i].length - 1);
+            System.out.printf("| = | (%10.4f) |\n", matriz[i][matriz[i].length - 1]);
+        }
+    }
+
+    public static void impresionCabecera(double[] soluciones) {
+        System.out.printf("%6sX%2s", " ", (1));
+        for (int i = 1; i < soluciones.length; i++) {
+            System.out.printf("%10sX%2s", " ", (i + 1));
+        }
+        for (int i = 0; i < soluciones.length - 1; i++) {
+            System.out.printf("%9sEa%2s", " ", (i + 1));
+        }
+        System.out.printf("%9sEa%2s\n", " ", (soluciones.length));
+
+        for (int i = 0; i < soluciones.length - 1; i++) {
+            System.out.printf("%10.4f   ", soluciones[i]);
+        }
+        System.out.printf("%10.4f\n", soluciones[soluciones.length - 1]);
+    }
+
+    public static void impresionLineas(double[] soluciones, double[] ea) {
+        for (int i = 0; i < soluciones.length; i++) {
+            System.out.printf("%10.4f   ", soluciones[i]);
+        }
+        for (int i = 0; i < ea.length - 1; i++) {
+            System.out.printf("%10.4f   ", ea[i]);
+        }
+        System.out.printf("%10.4f\n", ea[ea.length - 1]);
+    }
+
+    public static void despliegaSolucion(double[] soluciones) {
+        System.out.println("Las soluciones del sistema de ecuaciones simultáneas es: ");
+        for (int i = 0; i < soluciones.length; i++) {
+            System.out.printf("X%2s = %10.4f |\n", (i + 1), soluciones[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        double[][] matriz = leeEcuaciones();
+        GaussSeidel(matriz);
+    }
+
 }
