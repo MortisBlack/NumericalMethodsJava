315804f9a18c344b40ecdf01e09823bf034a901e
MortisBlack <jesus.urrego.iuc@gmail.com>
2021-11-07 17:17:06 -0700

    Lagrange polynomial

--- interpolacionLagrange/src/interpolacionlagrange/InterpolacionLagrange.java
+++ interpolacionLagrange/src/interpolacionlagrange/InterpolacionLagrange.java
@@ -5,76 +5,84 @@ import java.util.Scanner;
 public class InterpolacionLagrange {
 
 
-    public static double[][] leePuntos() {
+    public static void leePuntos() {
         Scanner scn = new Scanner(System.in);
-
-        double[][] puntos = new double[4][2];
-
+        
+        double[][] puntos;
+        double abscisa;
+        int grado;
+        boolean flag;
+        
+        System.out.println("Ingrese el valor de la abscisa:");
+        System.out.print("> "); 
+        abscisa = scn.nextDouble();
+        System.out.println("");
+        
+        do {
+            flag = true;
+            System.out.println("Ingrese el grado del polinomio (Máximo 10):");
+            System.out.print("> "); 
+            grado = scn.nextInt();
+            System.out.println("");
+            
+            if (grado >10) {
+                System.out.println("Grado incorrecto (Máximo 10).");
+                flag = false;
+            }
+        } while (!flag);
+        
+        puntos = new double[grado][2];
+        
         for (int i = 0; i < puntos.length; i++) {
             System.out.println("Ingrese el valor de X del punto numero " + (i + 1));
             System.out.print("> ");
             puntos[i][0] = scn.nextDouble();
-            System.out.println("");
-
             System.out.println("Ingrese el valor de Y del punto numero " + (i + 1));
             System.out.print("> ");
             puntos[i][1] = scn.nextDouble();
             System.out.println("");
         }
         
-        return puntos;
+        interpolacionlagrange(puntos, abscisa);
     }
     
-    public static double[] interpolacionlagrange(double[][] puntos) {
-        Scanner scn = new Scanner(System.in);
+    public static void interpolacionlagrange(double[][] puntos, double abscisa) {        
+        double grado = 0;
         
-        System.out.println("Ingrese el valor de X que desea interpolar:");
-        System.out.print("> ");
-        double x  = scn.nextDouble();
-        System.out.println("");
-
-        double aux;
-        double arriba = 1;
-        double abajo = 1;
-        double total = 0;
-
+        double arriba;
+        double abajo;
+        
         for (int i = 0; i < puntos.length; i++) {
-            aux = puntos[i][0];
-
+            
             arriba = 1;
             abajo = 1;
+            
+            // linea de arriba
             for (int j = 0; j < puntos.length; j++) {
-                if (puntos[j][0] != aux) {
-                    // Linea de arriba en la división
-                    arriba = arriba * (x - puntos[j][0]);
-                    
-                    // Linea de abajo en la división
-                    abajo = abajo * (aux - puntos[j][0]);
+                if(i != j) {
+                    arriba *= abscisa - puntos[j][0];
                 }
-            }            
-            total = puntos[i][1] * (arriba / abajo) + total;
+            }
+            
+            // linea de abajo
+            for (int j = 0; j < puntos.length; j++) {
+                if(i != j) {
+                    abajo *= puntos[i][0] - puntos[j][0];
+                }
+            }
+            
+            grado += ((arriba / abajo) * puntos[i][1]); 
         }
         
-        double[] datos = new double[2];
-        
-        // La ordenada del punto a interpolar
-        datos[0] = total;
-        
-        // El punto a interpolar
-        datos[1] = x;
-        return datos;
+        despliegaSolucion(grado, abscisa);
     }
     
-    public static void despliegaSolucion(double[] solucion){
-        System.out.printf("La abscisa para el punto X = %.6f según los datos ingresados es:\n", solucion[1]);
-        System.out.printf("%1c(%.6f) = %.6f\n",'\u0192',  solucion[1], solucion[0]);
+    public static void despliegaSolucion(double grado, double abscisa){
+        System.out.printf("La solución para el punto X = %.6f según los datos ingresados es:\n", abscisa);
+        System.out.printf("%1c(%.6f) = %.6f\n",'\u0192',  abscisa, grado);
     }
     
-    public static void main(String[] args) {
-        
-        double puntos[][] = leePuntos();
-        double[] datos = interpolacionlagrange(puntos);
-        despliegaSolucion(datos);
+    public static void main(String[] args) {     
+        leePuntos();
     }
-
 }
